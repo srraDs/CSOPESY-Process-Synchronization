59405ee1be6f93f321773840aa9504c11e0e73b3
Jvvvvv20 <84612741+Jvvvvv20@users.noreply.github.com>
2023-04-17 19:20:17 +0800

    Update test.py
    Changes

--- test.py
+++ test.py
@@ -1,101 +1,116 @@
-import threading
+import threading 
 import time
+import random
 
 class FittingRoom:
-    def __init__(self, n):
-        self.room = threading.BoundedSemaphore(n)
-        self.mutex = threading.Lock()
-        self.blue_done = 0
-        self.green_done = 0
-        self.switch = n
-        self.id = 1
-    
-    def fit_roomg(self):
-        # Fitting room thread for green
-        print("\n ID: "+ str(self.id) + " Color: Green")
-        self.green_done += 1
-        self.id += 1
-        time.sleep(3)
+    def __init__(self, capacity):
+        self.room = threading.BoundedSemaphore(capacity) 
+        self.mutex = threading.Lock() 
+        self.green_customers_done = 0 
+        self.blue_customers_done = 0 
+        self.switch = capacity # How many green customers can enter before allowing blue to enter
+        self.customer_id = 1 
+       
+    def green_room(self):
+        # Fitting room thread for green customers
+        print("\n ID: "+ str(self.customer_id) + " Color: [Green]")
+        self.green_customers_done += 1
+        self.customer_id += 1
+        time.sleep(3) 
+        self.room.release() 
+        if self.room._value == self.switch: 
+            print("\nEmpty fitting room!")
+            
+    def blue_room(self):
+        # Fitting room thread for blue customers
+        print("\n ID: "+ str(self.customer_id) + " Color: Blue")
+        self.blue_customers_done += 1
+        self.customer_id += 1
+        time.sleep(3) 
         self.room.release()
-        if self.room._value == n:
-            print("\nEmpty fitting room")
-    
-    def fit_roomb(self):
-        # Fitting room thread for blue
-        print("\n ID: "+ str(self.id) + " Color: Blue")
-        self.blue_done += 1
-        self.id += 1
-        time.sleep(3)
-        self.room.release()
-        if self.room._value == n:
-            print("\nEmpty fitting room")
+        if self.room._value == self.switch:
+            print("\nEmpty fitting room!")
 
-    def green_func(self):
-        ctr = 0
-        while self.green_done != g:
+    def green(self):
+        counter = 0
+        while self.green_customers_done != green_customers: 
             self.mutex.acquire()
             print("Green Only")
-            temp = 1;
-            while temp == 1:
+            temp = True
+            while temp:
                 self.room.acquire()
-                threading.Thread(target=self.fit_roomg).start()
-                time.sleep(1)
-                if self.green_done != g:
-                    if ctr == self.switch - 1:
-                        if self.blue_done != b:
-                            while self.room._value != n:
+                threading.Thread(target=self.green_room).start()
+                time.sleep(1) 
+                if self.green_customers_done != green_customers: 
+                    if counter == self.switch - 1: 
+                        if self.blue_customers_done != blue_customers:
+                            while self.room._value != capacity:
                                 pass
-                            self.mutex.release()
+                            self.mutex.release() 
                             time.sleep(1)
-                            temp = 0
-                            ctr = 0
+                            temp = False 
+                            counter = 0 
                     else:
-                        ctr += 1
-                elif self.green_done == g:
-                    ctr = 0
-                    while self.room._value != n:
+                        counter += 1 
+                elif self.green_customers_done == green_customers:
+                    counter = 0
+                    while self.room._value != capacity: 
                         pass
                     self.mutex.release()
                     time.sleep(1)
-                    temp = 0
+                    temp = False 
 
-    def blue_func(self):
-        ctr = 0
-        while self.blue_done != b:
+    def blue(self):
+        counter = 0
+        while self.blue_customers_done != blue_customers:
             self.mutex.acquire()
             print("Blue Only")
-            temp = 1;
-            while temp == 1:
+            temp = True
+            while temp:
                 self.room.acquire()
-                threading.Thread(target=self.fit_roomb).start()
+                threading.Thread(target=self.blue_room).start()
                 time.sleep(1)
-                if self.blue_done != b:
-                    if ctr == self.switch - 1:
-                        if self.green_done != g:
-                            while self.room._value != n:
+                if self.blue_customers_done != blue_customers:
+                    if counter == self.switch - 1:
+                        if self.green_customers_done != green_customers:
+                            while self.room._value != capacity:
                                 pass
                             self.mutex.release()
                             time.sleep(1)
-                            temp = 0
-                            ctr = 0
+                            temp = False
+                            counter = 0
                     else:
-                        ctr += 1
-                elif self.blue_done == b:
-                    ctr = 0
-                    while self.room._value != n:
+                        counter += 1
+                elif self.blue_customers_done == blue_customers:
+                    counter = 0
+                    while self.room._value != capacity:
                         pass
                     self.mutex.release()
                     time.sleep(1)
-                    temp = 0
-
+                    temp = False
+                    
 if __name__ == "__main__":
-    n,b,g=map(int, input("Enter n , b , g  values: ").split())
+    capacity, blue_customers, green_customers = map(int, input("n,b,g :").split())
 
-    room = FittingRoom(n)
+    room = FittingRoom(capacity)
 
-    green = threading.Thread(target=room.green_func)
-    blue = threading.Thread(target=room.blue_func)
+    # Generate a random number between 0 and 1
+    start_green = random.randint(0, 1)
+
+    if start_green:
+        green = threading.Thread(target=room.green, daemon=True)
+        blue = threading.Thread(target=room.blue, daemon=True)
+    else:
+        green = threading.Thread(target=room.blue, daemon=True)
+        blue = threading.Thread(target=room.green, daemon=True)
 
     green.start()
     time.sleep(1)
     blue.start()
+
+    # join threads
+    green.join()
+    blue.join()
+
+
+
